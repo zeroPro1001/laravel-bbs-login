47d243b48cfd61ab4f1f24442ef41de1d0052e4b
owner <owner@ownernoMacBook-Pro.local>
2019-06-02 16:46:34 +0900

    fitst commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PostsController@index')->name('top');
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
