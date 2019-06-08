64781be5dc11812243346b73c99275dbd2a26a1c
owner <owner@ownernoMacBook-Pro.local>
2019-06-08 15:18:17 +0900

    fifthcommit

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 
-Route::get('/', 'PostsController@index')->name('top');
+/*Route::get('/', 'PostsController@index')->name('top');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store']]);
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
 Route::resource('comments', 'CommentsController', ['only' => ['store']]);
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update']]);
-Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);*/
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -24,3 +24,8 @@ Route::get('/', function () {
 	return view('top');
 });
 
+Route::group(['middleware' => 'auth'], function () {
+Route::resource('users', 'UsersController');
+Route::resource('posts', 'PostsController',['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
+});
